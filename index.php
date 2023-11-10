332aa3e72e713c5e29077a30561e9c71c7a3d802
fakman99 <fatihakman1999@gmail.com>
2023-11-10 20:19:31 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="d-flex justify-content-center">
+    <div class="list-group">
+    <a href="assets/info_pratique.pdf" class="list-group-item list-group-item-action" download>Informations pratiques (.pdf)</a>
+    <a href="#" class="list-group-item list-group-item-action"download>TEST 2</a>
+    <a href="#" class="list-group-item list-group-item-action"download>TEST 3</a>
+
+    </div>
+</div>
+
+</body>
+</html>
